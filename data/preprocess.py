27c61815d89813f4ca999782f55d52d1a446a6cd
Jeff Wiroj <pasawee2@gmail.com>
2023-05-23 22:18:18 -0700

    Added get GloVe embedding logic

--- data/preprocess.py
+++ data/preprocess.py
@@ -1,6 +1,9 @@
 from tqdm import tqdm
 import spacy
 nlp = spacy.blank("en")
+from torchtext.vocab import GloVe
+import torch
+import torch.nn as nn
 
 def word_tokenize(sent):
     doc = nlp(sent)
@@ -27,8 +30,49 @@ def get_vocabs(wordSet=set(), charSet=set(), articles=None):
         wordSet.update(tokenized_question)
         charSet.update(context)
         charSet.update(question)
-        
+
     return wordSet, charSet
+
+def get_word2idx_embedding(wordSet=None, dim=50):
+    """ Create word2idx dict
+    word2idx is a dict from our wordSet to the index of the Glove Embedding
+    args:
+        wordSet
+        dim (int): dimension of GloVe
+    """
+    glove = GloVe(name='6B', dim=dim)
+    special_tokens = ["<UNK>","<PAD>"]
+
+    word2idx = {word: i for i, word in enumerate(glove.itos) if word in wordSet}
+
+    numNormalWords = len(word2idx.keys())
+    for i,word in special_tokens:
+        word2idx[word] = numNormalWords+i
+
+    vocab_size = len(word2idx.keys()) + len(special_tokens)
+    custom_embedding = torch.zeros((vocab_size,dim))
+
+    for i, token in enumerate(special_tokens):
+        custom_embedding[i + numNormalWords] = torch.randn(dim)
+
+    for word,idx in word2idx.items():
+        custom_embedding[idx] = glove.vectors[glove.stoi[word]]
+    
+    embedding_layer = nn.Embedding.from_pretrained(custom_embedding)
+    # Freeze everything up to special token
+    embedding_layer.weight[:numNormalWords].requires_grad = False
+
+    # with open(emb_file,'r') as f:
+    #     emb_dict = {}
+    #     for line in f:
+    #         # elem[0] is the word
+    #         # elem[1:] is the embedding
+    #         elem = line.split()
+    #         word,emb = elem[0], elem[1:]
+    #         if word in wordSet:
+    #             emb_dict[word] = emb
+    #     word2ind = {token:idx for idx,token in enumerate(emb_dict.keys())}
+    return word2idx, embedding_layer
 
 if __name__ == "__main__":
     charSet = set()
