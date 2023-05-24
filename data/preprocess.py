d4e78c597ea2d6a1772f75e13e8231205db817f8
Jeff Wiroj <pasawee2@gmail.com>
2023-05-24 09:46:55 -0700

    Changed preprocess logic

--- data/preprocess.py
+++ data/preprocess.py
@@ -1,15 +1,19 @@
 from tqdm import tqdm
 import spacy
+
 nlp = spacy.blank("en")
 from torchtext.vocab import GloVe
 import torch
 import torch.nn as nn
 
+
 def word_tokenize(sent):
     doc = nlp(sent)
     return [token.text for token in doc]
 
+
 CONTEXT_MAX_LEN = 400
+
 
 def get_vocabs(wordSet=set(), charSet=set(), articles=None):
     """
@@ -18,9 +22,10 @@ def get_vocabs(wordSet=set(), charSet=set(), articles=None):
     articles:
     """
     for it, article in enumerate(tqdm(articles)):
-        if len(article["context"]) >= CONTEXT_MAX_LEN: continue
-        context,question = article["context"], article['question']
-        context  = context.replace("''", '" ').replace("``", '" ')
+        if len(article["context"]) >= CONTEXT_MAX_LEN:
+            continue
+        context, question = article["context"], article["question"]
+        context = context.replace("''", '" ').replace("``", '" ')
         question = question.replace("''", '" ').replace("``", '" ')
 
         tokenized_context = word_tokenize(context)
@@ -33,50 +38,47 @@ def get_vocabs(wordSet=set(), charSet=set(), articles=None):
 
     return wordSet, charSet
 
+
 def get_word2idx_embedding(wordSet=None, dim=50):
-    """ Create word2idx dict
-    word2idx is a dict from our wordSet to the index of the Glove Embedding
+    """Create word2idx dict
+    word2idx is a dict from our wordSet and some special tokens to
+    the index of the embedding matrix.
     args:
-        wordSet
+        wordSet: All possible words in corpus
         dim (int): dimension of GloVe
     """
-    glove = GloVe(name='6B', dim=dim)
-    special_tokens = ["<UNK>","<PAD>"]
+    glove = GloVe(name="6B", dim=dim)
+    special_tokens = ["<UNK>", "<PAD>"]
 
-    word2idx = {word: i for i, word in enumerate(glove.itos) if word in wordSet}
+    word2idx = {}
+
+    idx = 0
+    for word in glove.stoi.keys():
+        if word in wordSet:
+            word2idx[word] = idx
+            idx += 1
 
     numNormalWords = len(word2idx.keys())
-    for i,word in special_tokens:
-        word2idx[word] = numNormalWords+i
+    for i, word in special_tokens:
+        word2idx[word] = numNormalWords + i
 
-    vocab_size = len(word2idx.keys()) + len(special_tokens)
-    custom_embedding = torch.zeros((vocab_size,dim))
+    vocab_size = len(word2idx.keys())
+    custom_embedding = torch.zeros((vocab_size, dim))
 
-    for i, token in enumerate(special_tokens):
-        custom_embedding[i + numNormalWords] = torch.randn(dim)
+    for idx, token in enumerate(special_tokens):
+        custom_embedding[idx + numNormalWords] = torch.randn(dim)
 
-    for word,idx in word2idx.items():
+    for word, idx in word2idx.items():
         custom_embedding[idx] = glove.vectors[glove.stoi[word]]
-    
-    embedding_layer = nn.Embedding.from_pretrained(custom_embedding)
-    # Freeze everything up to special token
-    embedding_layer.weight[:numNormalWords].requires_grad = False
 
-    # with open(emb_file,'r') as f:
-    #     emb_dict = {}
-    #     for line in f:
-    #         # elem[0] is the word
-    #         # elem[1:] is the embedding
-    #         elem = line.split()
-    #         word,emb = elem[0], elem[1:]
-    #         if word in wordSet:
-    #             emb_dict[word] = emb
-    #     word2ind = {token:idx for idx,token in enumerate(emb_dict.keys())}
+    embedding_layer = nn.Embedding.from_pretrained(custom_embedding)
+    # Parameters are stored in embedding_layer.weight
     return word2idx, embedding_layer
+
+
+# def get_char2idx_embedding(charSet,dim=16):
 
 if __name__ == "__main__":
     charSet = set()
     charSet.update("abs")
     print(charSet)
-
-
