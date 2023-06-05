16110c34cf8fbd45f57b1d91adc582d2ddbb1e60
Pisit Wajanasara <compisit1234@gmail.com>
2023-06-05 11:18:56 -0700

    Forward pass for evaluation and extract prediction result in BERT

--- train_bert.py
+++ train_bert.py
@@ -1,5 +1,7 @@
-import trainer
 import torch
+import collections
+import numpy as np
+import evaluate
 from data.dataset import SQuADBert
 from transformers import AutoModelForQuestionAnswering
 from torch.utils.data import DataLoader
@@ -7,6 +9,12 @@ from torch.optim import AdamW
 from transformers import default_data_collator, get_scheduler
 from accelerate import Accelerator
 from tqdm import tqdm
+
+def calculate_exact_match():
+    return 1.0
+
+def calculate_f1():
+    return 1.0
 
 def forward_one_step(model, inputs, accelerator, optimizer, lr_scheduler):
     outputs = model(**inputs)
@@ -31,7 +39,7 @@ def train_one_epoch(model, dataloader, accelerator, optimizer, lr_scheduler):
         progress_bar.update(1)
 
 
-def evaluate(model, dataloader):
+def evaluate_model(model, dataloader):
     with torch.no_grad():
         pass
 
@@ -51,12 +59,15 @@ def train_model(model, train_set, accelerator, optimizer, num_train_epochs=5):
         print()
 
 
-squad_train = SQuADBert("train[100:200]", "distilbert-base-uncased")
-squad_val = SQuADBert("validation[10:50]", "distilbert-base-uncased")
-print(squad_train[1:2])
-print(squad_val[1:2])
+model_name = "distilbert-base-uncased"
+model_name = "distilbert-base-cased-distilled-squad"
+squad_train = SQuADBert("train[100:200]", model_name)
+squad_val = SQuADBert("validation[:10]", model_name)
+print(squad_val.dataset["answers"])
+# print(squad_train[1:2])
+# print(squad_val[1:2])
 
-model = AutoModelForQuestionAnswering.from_pretrained("distilbert-base-uncased")
+model = AutoModelForQuestionAnswering.from_pretrained(model_name)
 optimizer = AdamW(model.parameters(), lr=2e-5)
 accelerator = Accelerator()
 
@@ -67,6 +78,54 @@ with torch.no_grad():
     model.eval()
     outputs = model(**squad_val_for_model[:])
 
-    print("pred: ", outputs[0])
+    start_logits = outputs.start_logits.cpu().numpy()
+    end_logits = outputs.end_logits.cpu().numpy()
+
+    example_to_features = collections.defaultdict(list)
+    for idx, feature in enumerate(squad_val.tokenized_dataset):
+        example_to_features[feature["example_id"]].append(idx)
+
+    n_best = 10
+    max_answer_length = 30
+    predicted_answers = []
+
+    for example in squad_val.dataset:
+        example_id = example["id"]
+        context = example["context"]
+        answers = []
+
+        for feature_idx in example_to_features[example_id]:
+            start_logit = start_logits[feature_idx]
+            end_logit = end_logits[feature_idx]
+            offsets = squad_val.tokenized_dataset["offset_mapping"][feature_idx]
+
+            start_indexes = np.argsort(start_logit)[-1: -n_best - 1: -1].tolist()
+            end_indexes = np.argsort(end_logit)[-1: -n_best - 1: -1].tolist()
+
+            for start_index in start_indexes:
+                for end_index in end_indexes:
+                    # Skip answers that are not fully in the context
+                    if offsets[start_index] is None or offsets[end_index] is None:
+                        continue
+                    # Skip answers with a length that is either < 0 or > max_answer_length
+                    if (end_index < start_index) or (end_index - start_index + 1 > max_answer_length):
+                        continue
+
+                    answers.append({"text": context[offsets[start_index].cpu()[0].item(): offsets[end_index].cpu()[1].item()],
+                                    "logit_score": start_logit[start_index] + end_logit[end_index]})
+        
+        best_answer = max(answers, key=lambda x: x["logit_score"])
+        predicted_answers.append({"id": example_id, "prediction_text": best_answer["text"]})
+    
+    print(predicted_answers)
+
+    theoretical_answers = [{"id": ex["id"], "answers": ex["answers"]} for ex in squad_val.dataset]
+    print(theoretical_answers)
+
+    metric = evaluate.load("squad")
+    eval_result = metric.compute(predictions=predicted_answers, references=theoretical_answers)
+
+    print(eval_result)
+
 
 train_model(model, train_set=squad_train, accelerator=accelerator, optimizer=optimizer)
