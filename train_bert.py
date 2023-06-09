df2aa31abb9ea9c54c3244a8969a316ad96f6a11
Pisit Wajanasara <compisit1234@gmail.com>
2023-06-08 22:58:20 -0700

    First version of code for training

--- train_bert.py
+++ train_bert.py
@@ -2,7 +2,6 @@ import torch
 import collections
 import numpy as np
 
-# import evaluate
 from data.dataset import SQuADBert
 from transformers import AutoModelForQuestionAnswering
 from torch.utils.data import DataLoader
@@ -17,8 +16,8 @@ def calculate_exact_match(predicted_answers, theoretical_answers):
     for predicted_answer, theoretical_answer in zip(
         predicted_answers, theoretical_answers
     ):
-        print(predicted_answer)
-        print(theoretical_answer)
+        # print(predicted_answer)
+        # print(theoretical_answer)
         pred = predicted_answer["prediction_text"]
         for answer in theoretical_answer["answers"]["text"]:
             if pred == answer:
@@ -42,20 +41,29 @@ def calculate_f1(predicted_answers, theoretical_answers):
             current_tp = len(pred_tokens.intersection(answer_tokens))
             current_fp = len(pred_tokens.difference(answer_tokens))
             current_fn = len(answer_tokens.difference(pred_tokens))
-            current_precision = current_tp / (current_tp + current_fp)
-            current_recall = current_tp / (current_tp + current_fn)
-            current_f1 = (
-                2
-                * current_precision
-                * current_recall
-                / (current_precision + current_recall)
-            )
+            if current_tp + current_fp == 0:
+                current_precision = 0
+            else:
+                current_precision = current_tp / (current_tp + current_fp)
+            if current_tp + current_fp == 0:
+                current_recall == 0
+            else:
+                current_recall = current_tp / (current_tp + current_fn)
+            if current_recall + current_precision == 0:
+                current_f1 = 0
+            else:
+                current_f1 = (
+                    2
+                    * current_precision
+                    * current_recall
+                    / (current_precision + current_recall)
+                )
             if current_f1 > best_f1:
                 best_f1 = current_f1
         if len(pred) == 0 and len(theoretical_answers["answers"]["text"]) == 0:
             best_f1 = 1.0
         overall_f1 += best_f1
-        print(best_f1)
+        # print(best_f1)
     return overall_f1 / len(predicted_answers)
 
 
@@ -69,6 +77,8 @@ def forward_one_step(model, inputs, accelerator, optimizer, lr_scheduler):
     lr_scheduler.step()
     optimizer.zero_grad()
 
+    return loss.item()
+
 
 def train_one_epoch(model, dataloader, accelerator, optimizer, lr_scheduler):
     # Train step
@@ -77,96 +87,39 @@ def train_one_epoch(model, dataloader, accelerator, optimizer, lr_scheduler):
     num_training_steps = len(dataloader)
     progress_bar = tqdm(range(num_training_steps))
 
+    total_loss = 0.0
     for step, batch in enumerate(dataloader):
-        forward_one_step(model, batch, accelerator, optimizer, lr_scheduler)
+        total_loss += forward_one_step(
+            model, batch, accelerator, optimizer, lr_scheduler
+        )
         progress_bar.update(1)
 
-
-def evaluate_model(model, dataloader):
-    with torch.no_grad():
-        pass
+    return total_loss / len(dataloader)
 
 
-def train_model(model, train_set, accelerator, optimizer, num_train_epochs=5):
-    train_dataloader = DataLoader(
-        train_set, shuffle=True, collate_fn=default_data_collator, batch_size=8
-    )
-    model, optimizer, train_dataloader = accelerator.prepare(
-        model, optimizer, train_dataloader
-    )
-
-    num_update_steps_per_epoch = len(train_dataloader)
-    num_training_steps = num_train_epochs * num_update_steps_per_epoch
-
-    lr_scheduler = get_scheduler(
-        "linear",
-        optimizer=optimizer,
-        num_warmup_steps=0,
-        num_training_steps=num_training_steps,
-    )
-
-    for epoch in range(num_train_epochs):
-        print("=" * 10 + f"Epoch {epoch + 1}", "=" * 10)
-        train_one_epoch(model, train_dataloader, accelerator, optimizer, lr_scheduler)
-        print()
-
-
-model_name = "distilbert-base-uncased"
-model_name = "distilbert-base-cased-distilled-squad"
-squad_train = SQuADBert("train[100:200]", model_name)
-squad_val = SQuADBert("validation[:10]", model_name)
-print(squad_val.dataset["answers"])
-# print(squad_train[1:2])
-# print(squad_val[1:2])
-
-model = AutoModelForQuestionAnswering.from_pretrained(model_name)
-optimizer = AdamW(model.parameters(), lr=2e-5)
-accelerator = Accelerator()
-device = "mps:0"
-
-with torch.no_grad():
-    squad_val_for_model = squad_val.tokenized_dataset.remove_columns(
-        ["example_id", "offset_mapping"]
-    )
-    # squad_val_for_model = squad_val.tokenized_dataset
-    squad_val_for_model.set_format("torch")
-    # model, squad_val_for_model = accelerator.prepare(model, squad_val_for_model)
-    model = accelerator.prepare(model)
-    # print(squad_val_for_model["input_ids"])
+@torch.no_grad()
+def predict_answers(model, dataloader, val_set):
     model.eval()
-    # print("="*20)
-    # print({accelerator.prepare(v).device for k, v in squad_val_for_model[:].items()})
-    # print("="*20)
-    # print(model.device)
-    batch_size = 2
-    val_dataloader = DataLoader(
-        squad_val_for_model,
-        shuffle=False,
-        collate_fn=default_data_collator,
-        batch_size=batch_size,
-    )
     predicted_answers = []
 
     start_logits = []
     end_logits = []
 
-    for step, batch in enumerate(val_dataloader):
-        # features = batch.remove_columns(["example_id", "offset_mapping"])
-        features = {k: v.to(device) for k, v in batch.items()}
-        # batch = {k: accelerator.prepare(v) for k, v in batch.items()}
+    for step, batch in enumerate(dataloader):
+        features = {k: v.to(model.device) for k, v in batch.items()}
         outputs = model(**features)
 
         start_logits += list(outputs.start_logits.cpu().numpy())
         end_logits += list(outputs.end_logits.cpu().numpy())
 
     example_to_features = collections.defaultdict(list)
-    for idx, feature in enumerate(squad_val):
+    for idx, feature in enumerate(val_set):
         example_to_features[feature["example_id"]].append(idx)
 
     n_best = 10
     max_answer_length = 30
 
-    for example in squad_val.dataset:
+    for example in val_set.dataset:
         example_id = example["id"]
         context = example["context"]
         answers = []
@@ -174,7 +127,7 @@ with torch.no_grad():
         for feature_idx in example_to_features[example_id]:
             start_logit = start_logits[feature_idx]
             end_logit = end_logits[feature_idx]
-            offsets = squad_val.tokenized_dataset["offset_mapping"][feature_idx]
+            offsets = val_set.tokenized_dataset["offset_mapping"][feature_idx]
 
             start_indexes = np.argsort(start_logit)[-1 : -n_best - 1 : -1].tolist()
             end_indexes = np.argsort(end_logit)[-1 : -n_best - 1 : -1].tolist()
@@ -208,20 +161,88 @@ with torch.no_grad():
             predicted_answers.append(
                 {"id": example_id, "prediction_text": best_answer["text"]}
             )
+    return predicted_answers
 
-    # print(predicted_answers)
 
+@torch.no_grad()
+def evaluate_model(model, dataloader, val_set):
+    predicted_answers = predict_answers(model, dataloader, val_set)
     theoretical_answers = [
-        {"id": ex["id"], "answers": ex["answers"]} for ex in squad_val.dataset
+        {"id": ex["id"], "answers": ex["answers"]} for ex in val_set.dataset
     ]
-    # print(theoretical_answers)
-
-    # metric = evaluate.load("squad")
-    # eval_result = metric.compute(predictions=predicted_answers, references=theoretical_answers)
 
     em = calculate_exact_match(predicted_answers, theoretical_answers)
     f1 = calculate_f1(predicted_answers, theoretical_answers)
-    print(em, f1)
+
+    return {"em": em, "f1": f1}
 
 
-# train_model(model, train_set=squad_train, accelerator=accelerator, optimizer=optimizer)
+def train_model(
+    model,
+    train_set,
+    val_set,
+    accelerator,
+    optimizer,
+    num_train_epochs=5,
+    val_batch_size=20,
+):
+    train_dataloader = DataLoader(
+        train_set, shuffle=True, collate_fn=default_data_collator, batch_size=16
+    )
+    val_for_model = val_set.tokenized_dataset.remove_columns(
+        ["example_id", "offset_mapping"]
+    )
+    val_for_model.set_format("torch")
+    val_dataloader = DataLoader(
+        val_for_model,
+        shuffle=False,
+        collate_fn=default_data_collator,
+        batch_size=val_batch_size,
+    )
+
+    model, optimizer, train_dataloader = accelerator.prepare(
+        model, optimizer, train_dataloader
+    )
+
+    num_update_steps_per_epoch = len(train_dataloader)
+    num_training_steps = num_train_epochs * num_update_steps_per_epoch
+
+    lr_scheduler = get_scheduler(
+        "linear",
+        optimizer=optimizer,
+        num_warmup_steps=0,
+        num_training_steps=num_training_steps,
+    )
+
+    for epoch in range(num_train_epochs):
+        print("=" * 10 + f"Epoch {epoch + 1}", "=" * 10)
+        loss = train_one_epoch(
+            model, train_dataloader, accelerator, optimizer, lr_scheduler
+        )
+        eval_result = evaluate_model(
+            model=model, dataloader=val_dataloader, val_set=val_set
+        )
+        print(f"Loss: {loss}")
+        print(f"Evaluation: EM={eval_result['em']}, F1={eval_result['f1']}")
+        print()
+
+
+if __name__ == "__main__":
+    model_name = "distilbert-base-uncased"
+    # model_name = "distilbert-base-cased-distilled-squad"
+    squad_train = SQuADBert("train[:100]", model_name)
+    val_set = SQuADBert("validation[:100]", model_name)
+    # print(squad_train[1:2])
+    # print(squad_val[1:2])
+
+    model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+    optimizer = AdamW(model.parameters(), lr=2e-5)
+    accelerator = Accelerator()
+
+    train_model(
+        model,
+        train_set=squad_train,
+        val_set=val_set,
+        accelerator=accelerator,
+        optimizer=optimizer,
+    )
